014c662fc4e62d2be2023507aee863e082ffd14e
Annie Berjou <annie.carrau@wanadoo.fr>
2026-01-11 22:25:55 +0100

    création des différentes pages

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,17 @@
+import "./Home.css";
+import logo from "../../assets/imageSource1.png";
+
 function Home() {
-  return <h1>Accueil</h1>;
+  return (
+    <div className="home-container">
+      <div className="home-banner">
+        <h1>Accueil</h1>
+        <img src={logo} alt="Falaise bord de mer" />
+        <p>Chez vous, partout et ailleurs</p>
+      </div>
+      <div className="home-gallery"></div>
+    </div>
+  );
 }
 
 export default Home;
