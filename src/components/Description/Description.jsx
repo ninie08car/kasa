b9a3bafe27bc82f863c7a63a1089a7dd5b5c9499
Annie Berjou <annie.carrau@wanadoo.fr>
2026-01-23 20:50:03 +0100

    réalisation scss Description

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -6,34 +6,36 @@ function HousingDescription() {
   const { id } = useParams();
 
   const housing = housings.find((item) => item.id === id);
+  const { title, location, tags, host, rating } = housing;
+
   return (
     <div className="housing">
-      <div className="housing-left">
+      <div className="housing-top">
         <div className="housing-content">
-          <h1>{housing.title}</h1>
-          <p>{housing.location}</p>
+          <h1>{title}</h1>
+          <p className="location">{location}</p>
         </div>
-        <div className="housing-tags">
-          <ul>
-            {housing.tags.map((tag, index) => (
-              <li key={index}>{tag}</li>
-            ))}
-          </ul>
+        <div className="housing-host">
+          <p>{host.name}</p>
+          <div className="round-container">
+            <img src={host.picture} alt={host.name} />
+          </div>
         </div>
       </div>
-      <div className="housing-right">
-        <div className="housing-host">
-          <p>{housing.host.name}</p>
-          <img src={housing.host.picture} alt={housing.host.name} />
+      <div className="housing-bottom">
+        <div className="housing-tags">
+          {tags.map((tag, index) => (
+            <div key={index} className="tag">
+              {tag}
+            </div>
+          ))}
         </div>
         <div className="housing-rating">
           {[1, 2, 3, 4, 5].map((star) => (
             <i
               key={star}
               className={
-                star <= housing.rating
-                  ? "fa-solid fa-star active"
-                  : "fa-solid fa-star"
+                star <= rating ? "fa-solid fa-star active" : "fa-solid fa-star"
               }
             ></i>
           ))}
